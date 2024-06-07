ce7a229242ad86806a5a6f1814e5a85b45cd55bd
nguyenvu852004 <nguyenvu852004@gmail.com>
2024-06-07 13:37:16 +0700

    run HelloWorld

---
+++ src/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT
+const hostname = process.env.HOST_NAME
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
